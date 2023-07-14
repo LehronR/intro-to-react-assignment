b09fa553d3795bbff1d9fc6f64dbba04e49a41a0
Lehron <lehronrobinson@yahoo.com>
2023-07-13 22:29:43 -0400

    Seperated data from app.jsx for better structure and cleaner files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,8 @@
 import "./styles/App.css";
+import users from './data/users';
 import UserProfile from './components/UserProfile';
+
 function App() {
-  //The array of users to render in the assignment
-  const users = [
-    {
-      name: "John Doe",
-      email: "john@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
-    },
-    {
-      name: "Jane Smith",
-      email: "jane@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
-    },
-    {
-      name: "Michael Johnson",
-      email: "michael@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
-    },
-    {
-      name: "Emily Brown",
-      email: "emily@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
-    },
-    {
-      name: "David Wilson",
-      email: "david@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
-    },
-    {
-      name: "Sarah Davis",
-      email: "sarah@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
-    },
-    {
-      name: "Daniel Thompson",
-      email: "daniel@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
-    },
-    {
-      name: "Olivia Martinez",
-      email: "olivia@example.com",
-      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
-    },
-  ];
   return (
     <div className="App">
       <header >
---
+++ src/data/users.jsx
@@ -0,0 +1,44 @@
+const users = [
+    {
+      name: "John Doe",
+      email: "john@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Jane Smith",
+      email: "jane@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "Michael Johnson",
+      email: "michael@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Emily Brown",
+      email: "emily@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "David Wilson",
+      email: "david@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Sarah Davis",
+      email: "sarah@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "Daniel Thompson",
+      email: "daniel@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Olivia Martinez",
+      email: "olivia@example.com",
+      imgUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+  ];
+
+  export default users;
